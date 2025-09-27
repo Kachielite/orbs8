d1e49b180a9319924cad7618820bd6792f5ce1d4
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-09-27 06:23:13 +0300

    feat(cors): configure CORS to allow specific origins and enable credentials

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,10 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document, {
     swaggerOptions: { persistAuthorization: true },
   });
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3001'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
