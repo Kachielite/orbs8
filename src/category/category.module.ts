f35aeaa11fc215ed267fb9e16a0a175ba33f2691
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-10-06 05:37:55 +0300

    feat(category): implement update functionality for category and transaction association

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -5,9 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { OpenAIConfig } from '../common/configurations/openai.config';
 import { CategoryFeedback } from './entities/category-feedback.entity';
+import { Transaction } from '../transaction/entities/transaction.entity';
+import { User } from '../auth/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category, CategoryFeedback])],
+  imports: [TypeOrmModule.forFeature([Category, CategoryFeedback, Transaction, User])],
   controllers: [CategoryController],
   providers: [CategoryService, OpenAIConfig],
 })
